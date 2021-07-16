e7843b288f08f698a0db78f552f5fc7d32bb6f6b
Raboniel <imalwaysfantastic@gmail.com>
2021-07-16 19:27:18 -0400

    confused

--- skeleton 2/movie_buff/02_queries.rb
+++ skeleton 2/movie_buff/02_queries.rb
@@ -32,9 +32,10 @@ end
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles. # , :name, "COUNT(ord)"
-  Actor.select(:id) 
+  Actor.select(:id, :name, "COUNT(ord) as roles") 
     .joins(:castings)
     .group(:id)
-    .having.not(ord: 1)
+    .having("MAX(ord)!=1")
+    .order("COUNT(ord) DESC")
     .limit(2)
 end
